6e286078aa7c0ee05d7e1bf0908ac887932240fe
duffydduffyd <denise@foodfeedback.com>
2016-11-15 16:50:42 -0800

    Initial commit and basic questions

---
+++ ruby/hamsters.rb
@@ -0,0 +1,23 @@
+
+
+# Release 1, Gather hamsters of unknown origin 
+
+puts "What is the gangster hamster name?"
+name = gets.chomp
+
+puts "How loud is your hamster 1-10?"
+volume = gets.chomp.to_i
+
+puts "What is the fur color"
+fur_color = gets.chomp
+
+puts "Is this hamster adoptable? (yes/no)"
+adoptable = gets.chomp
+
+puts "How old is our hamster?"
+age = gets.chomp.to_f
+
+
+
+
+
