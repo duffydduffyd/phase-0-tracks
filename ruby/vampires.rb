2684ab901fd912802acba870649ca228b29d11ae
duffydduffyd <denise@foodfeedback.com>
2016-11-17 08:50:17 -0800

    Complete hamsters refactor vampires

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -5,7 +5,7 @@ garlic_bread = nil
 health_insurance = nil
 current_allergies = nil
 
-	# This is a loop that asks how many will be processed
+# This is a loop that asks how many will be processed
 	puts "How many employees will you process in this batch?"
 	number_of_employees = gets.to_i
 	# other way is gets.chomp.to_i
@@ -26,56 +26,66 @@ current_allergies = nil
 		year_born = gets.to_i
 		p year_born
 
+		# 
 		puts "Our company cafeteria serves garlic bread. Should we order some for you? (y/n)"
 		garlic_bread = gets.chomp
+			if garlic_bread[0] == "Y" 
+					garlic_bread = true
+				elsif garlic_bread[0] == "y"
+					garlic_bread = true
+				elsif garlic_bread[0] == "N" 
+				 	garlic_bread[0] = false
+				elsif garlic_bread[0] == "n" 
+				garlic_bread = false	
+				else garlic_bread = nil
+			end	
 		p garlic_bread
 
 		puts "Would you like to enroll in the company’s health insurance? (y/n)"
 		health_insurance = gets.chomp
+			if health_insurance[0] == "Y" 
+					health_insurance = true
+				elsif health_insurance[0] == "y" 	
+					health_insurance = true
+				elsif health_insurance[0] == "N"
+					health_insurance = false
+				elsif health_insurance[0] == "n" 	
+					health_insurance = false
+				else health_insurance = nil	
+			end	
 		p health_insurance	
 
 		puts "Your name is #{employee_name}"
-		puts "Your age is #{current_age} ."
-		puts "The year you were born is #{year_born}" 
-		puts "Likes garlic bread? #{garlic_bread}"
-		puts "Has health insurance? #{health_insurance}"
-
-
+		puts "Your age is #{current_age}."
+		puts "The year you were born is #{year_born}." 
+		puts "#{employee_name} likes garlic bread: #{garlic_bread}"
+		puts "#{employee_name} has health insurance: #{health_insurance}"
 
 		# This area shows if there is a vampire conclusion based on age, name, garlic break affinity, and need for healthcare
-		if current_age >= 125
-			puts "You are most definately a vampire based on your current_age."
-		else 
-			puts "You are a bit young for most vampires."	
-		end
-
-		if (current_age <= 125 && year_born >= 1891) && (garlic_bread == "y" || health_insurance == "y")
-			puts "You are a probably not a vampire."
-		elsif employee_name == ("Drake Cula" || "Tu Fang")
-			puts "omg, your name gives it away. You are a Vampire!"
-		elsif (current_age >= 125 || year_born <= 1891) && (health_insurance == "n" || health_insurance == nil)
-			puts "You are a most probably a vampire."
-		elsif (current_age >= 125 || year_born <= 1891) && (garlic_bread == "n" || garlic_bread == nil) && (health_insurance == "n" || health_insurance == nil)
-			puts "You are almost certainly a vampire. Ahhhhh! Scary! Although diversity at work is nice. Let's just not work together after sundown."
-		else 
-			puts "Results Inconclusive."    
+		if (current_age <= 125 && year_born >= 1891) && (garlic_bread == true || health_insurance == true)
+				puts "You are not a vampire."
+			elsif employee_name == ("Drake Cula" || "Tu Fang")
+				puts "omg, your name gives it away. You are a Vampire!"
+			elsif (current_age >= 125 || year_born <= 1891) && (health_insurance == false) && (garlic_bread == false)
+				puts "You are certainly a vampire."
+			else 
+				puts "Results Inconclusive."    
 		end
 
 
-		# This area is attempting to use a while loop about alergies. 
-		while current_allergies != ("sunshine" || "done")
-			puts "What are your current allergies? Please list one at a time in lowercase and press return. Type 'done' when you have listed them all." 
-			current_allergies = gets.chomp
-
+		# This area is attempting to use a while loop about alergies.
+		puts "What are your current allergies? Type 'done' when you have listed them all." 
+		current_allergies = gets.chomp
+		if current_allergies == "sunshine"
+				puts "Wow, you actually are a vampire."		
+			elsif 
+				current_allergies == "done"
+				puts "Thank you for this information."
+			else	
+				current_allergies = gets.chomp
 		end
-			
-		if current_allergies == "done"
-			puts "Thank you for this information."
-		else
-			puts "You are pretty much a vampire."		
-		end	
-
-	number_of_employees -= 1 
+		
+		number_of_employees -= 1 
 	end
 
 puts "Actually, never mind! What do these questions have to do with anything? Let's all be friends."
