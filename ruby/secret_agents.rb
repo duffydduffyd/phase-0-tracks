ac7a62eb49a62eb93294eb63f36de5e8fa073424
duffydduffyd <denise@foodfeedback.com>
2016-11-18 16:38:56 -0800

    Final push of secret_agent

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -1,80 +1,48 @@
 def encrypt (string)
  string_length = string.length
- counter = 0
+ index = 0
  new_string = ""
-
-  while counter < string_length
-  	if string[counter] == "z"
-  		new_string += "a"
-  	elsif string[counter] == " "
-  		new_string += " "
-  	else			
-  		new_string += string[counter].next
-  	end  
-     counter += 1
+  while index < string_length
+    if string[index] == "z"
+        new_string += "a"
+    else            
+        new_string += string[index].next
+    end  
+     index += 1
   end   
-  	puts new_string
+    puts new_string
 end
 
-# encrypt("abc") --> "bcd"
-# encrypt ("zed") --> "afe"
-# encrypt("abcdefghijklmnopqrstuvwxyz") --> bcdefghijklmnopqrstuvwxyza
 
-# def decrypt(string)
-# index = -1
-# decrypt_length = string.length
-# counter = 0
-# decrypt_string = ""
-
-# 	while counter < decrypt_length
-# 			if string[index] == "a"
-# 	  		decrypt_string += "z"
-# 	  	elsif string[index] == " "
-# 	  		decrypt_string += " "
-# 	  	else
-# 	  		decrypt_string += string[index]
-# 			end
-# 			index = index - 1
-# 			counter += 1
-# 	end
-# 		puts decrypt_string
-# end
-
-# def decrypt(string)
-# 	alphabet = "abcdefghijklmnopqrstuvwxyz"
-# 	counter = 0
-# 	index = 0
-# 	decrypt_string = ""
-# 		while counter < string.length
-# 			decrypt_string += string[counter]
-# #			p current_letter
-# 			counter += 1
-# 		end
-# 		p decrypt_string	
-# end
-
-def decrypt(string)
-		alphabet = "abcdefghijklmnopqrstuvwxyz"
-		string_length = string.length
-		decrypt_string = ""
-
-		while counter < string_length
-# find out where the index of the string in postion 0 thru string_lenth is in the alphabet	 			
-		index = alphabet.index(string[counter].to_s)
-# subtract one letter from the alphabet
-		new_letter = alphabet[index - 1].to_s		
-	 			encrypt_letter = alphabet.index(decrypt_string[counter])
-	 			decrypt_string += string[counter]
- 				counter += 1
- 		end
-# 		p decrypt_string	
-# end
+def decrypt (string)
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+ string_length = string.length
+ index = 0
+ new_string = ""
+  while index < string_length
+      i = alphabet.index(string[index]) 
+      new_string += alphabet[i-1]
+      index += 1
+  end   
+    puts new_string
+end
 
 
+#Driver Code
 
+#this worked as the encrypt method because it used the last command opposed to the first
+#decrypt(encrypt("swordfish"))
 
+#ask user if they would like to decrypt or encrypt 
+puts "Would you like to decrypt or encrypt a password?"
+preference = gets.chomp
 
-decrypt("bcd")
-# decrypt("afe")
-# decrypt("abcdefghijklmnopqrstuvwxyz")
+#ask user for their password
+puts "What is your password?"
+password = gets.chomp
 
+    if preference == "decrypt"
+        puts decrypt(password)
+    else
+        puts encrypt(password)
+    end
