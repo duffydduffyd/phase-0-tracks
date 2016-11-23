9cb7e9ea6ca5e029daa7df31c311222e680153fd
duffydduffyd <denise@foodfeedback.com>
2016-11-22 17:13:58 -0800

    Add Rebecca's driver code

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -5,16 +5,16 @@
 
 # def greatest_artist
 # 	puts "Who is the greatist artist?"
-# 		# artist1 = "Picasso"
-# 		# artist2 = "Matisse"
-# 		# yield(artist1,artist2)
+	 		# artist1 = "Picasso"
+	 		# artist2 = "Matisse"
+	 		# yield(artist1,artist2)
 
-# 		yield ("Picasso"),("Renoir")
-# 		# yield ("Bechtal"),("Olivera")
-# 		# yield ("Rodin"),("Brancusi")
-	
-# 		# 3.times {yield}
+			# yield ("Picasso"),("Renoir")
+			# yield ("Bechtal"),("Olivera")
+			# yield ("Rodin"),("Brancusi")
 		
+			# 3.times {yield}
+			
 # end
 
 # greatest_artist { |artist1, artist2| puts "Is it #{artist1} or #{artist2}?" }		
@@ -25,9 +25,6 @@
 # Is it Picasso or Renoir?
 
 # Release 1 Use each, map, and map!
-# Demonstrate that you can iterate through each one using .each, and then 
-# using .map! (modifying the data in some way). Note that you can't call 
-# .map! on a hash, so you can skip that
 
 # art_movements = ["Impressionisism", "Modern", "Surrealism","Bay Area Figurative","Pop Art"]
 
@@ -39,30 +36,67 @@
 # 	:Andy_Warhol => "Marilyn_Monroe_number_251"
 #  }
 
-# # p art_movements
-# # p works_of_art
+# p art_movements
+# p works_of_art
 
-# # art_movements.each {|movement| puts "#{movement} is a art movement"} 
+# art_movements.each {|movement| puts "#{movement} is a art movement"} 
 
-# # Add the work movements to array
-# # puts art_movements
-# # art_movements.map! {|movement| movement+= " Movement"} 
-# # puts art_movements
+# Add the work movements to array
+# puts art_movements
+# art_movements.map! {|movement| movement+= " Movement"} 
+# puts art_movements
 
 
-# # puts works_of_art
-# # works_of_art.each {|artist,title| puts "#{title} by #{artist}" }
-# # puts works_of_art
+# puts works_of_art
+# works_of_art.each {|artist,title| puts "#{title} by #{artist}" }
+# puts works_of_art
 
-# # p works_of_art
-# # new_array = works_of_art.map {|artist,title| [title, title, artist] }
-#  # puts new_array
+# p works_of_art
+# new_array = works_of_art.map {|artist,title| [title, title, artist] }
+# puts new_array
 # puts works_of_art
 # new_array = works_of_art.map {|artist,title| ["#{title} was painted by #{artist}"] }
 # puts new_array
 
+list_of_integers = [0, 3, 10, 12, 15, 63, 84]
+places = {
+	:california => "san francisco",
+	:oregon => "portland",
+	:washington => "seattle"
+}
 
+# Arrays
+# 1
+# list_of_integers.delete_if { |num| num <= 10 }
+# puts list_of_integers
 
+# 2
+# new_array = list_of_integers.select {|num| num < 3}
+# puts new_array
+
+# 3
+# list_of_integers.reject! {|num| num % 2 == 0}
+# puts list_of_integers
+
+# 4
+# some_array = list_of_integers.take_while {|num| num < 10}
+# puts some_array
+
+# Hashes
+# 1
+# better_places = places.reject { |state, city| state == :oregon}
+# puts better_places
+
+# 2
+# long_city_hash = places.keep_if { | state, city| city.length > 7}
+# puts long_city_hash
+
+# 3
+# short_cities = places.keep_if {|state, city| city.length < 8}
+# puts short_cities
+
+# 4
+#puts places.fetch(:california) { |state| "oops that #{state} wasn't found"}
 
 
 
