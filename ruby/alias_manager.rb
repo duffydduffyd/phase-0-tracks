31565d6c62237156b42845bc61de3688c902c546
duffydduffyd <denise@foodfeedback.com>
2016-11-22 22:23:49 -0800

    Small progress on flipping names

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,15 +1,49 @@
 # 5.5 Solo Challenge: Manipulating Strings with Iteration 
 # by Denise Duffy 11/22/16
 
-real_name = Denise Duffy
+
+real_name = "Denise Duffy"
+p real_name # => "Denise Duffy"
 
 # Downcase the name
+# Swap first and last name.
+short_name = real_name.downcase # => "denise duffy"
+p short_name 
 
-# Swap first and last name. Split the word and exchange indexes. 
+flipped_name = short_name.split.reverse # => ["duffy", "denise"]
+p flipped_name
+
+# Figure out how to get this into an array of characters: => ["d","u","f","f","y"," ","d","e","n","i","s","e"]
+# No clear built in method to do this. 
+
+
+
+
+ # Change all letters to the next letter
+def next_letter (x)
+ 	puts "hello"
+end
+
+next_letter(a)
+
+# example a.each {|x| print x, " -- " }
+
+
+# real_name.each {|letter| letter.next }
+
+# real_name.map do |encrypt|
+# 	 	puts real_name.next
+# end
+
+
 
 # Change all the vowels to the next vowel
+# An array of vowels, compare one array to another
+# vowels = 'a e i o u'
+# p vowels.split
 
-# Change all consonant to the next consonant.
+
+
 
 # Upcase name
 
