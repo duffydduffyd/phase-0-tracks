1ab1ef5d084ebabf14bb1afa1de6ff8464d90d93
duffydduffyd <denise@foodfeedback.com>
2016-11-22 20:02:30 -0800

    add basic peudocode

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -1,2 +1,16 @@
 # 5.5 Solo Challenge: Manipulating Strings with Iteration 
 # by Denise Duffy 11/22/16
+
+real_name = Denise Duffy
+
+# Downcase the name
+
+# Swap first and last name. Split the word and exchange indexes. 
+
+# Change all the vowels to the next vowel
+
+# Change all consonant to the next consonant.
+
+# Upcase name
+
+# If ! then add a space kind of thing
