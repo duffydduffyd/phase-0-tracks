2e1e18a4f2cea0d03b22f4e2f3a64ff8aed0da7b
duffydduffyd <denise@foodfeedback.com>
2016-11-30 10:01:58 -0800

    Add standalone methods for yell_angrily and yell_happily

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,5 +1,13 @@
-# Denise Duffy 6.4 Modules 
+# Denise Duffy 6.4 Writing a Module 
 
 module Shout
-  # we'll put some methods here soon, but this code is fine for now!
+  
+  def self.yell_angrily(words)
+    words + "!!!" + " :("
+  end
+
+  def self.yell_happily(words)
+  	words.capitalize + "!" + "woo hoo!"
+  end	
+
 end
