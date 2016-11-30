6520345baaf1d6d8b95f078e7beac5c84a6daa85
duffydduffyd <denise@foodfeedback.com>
2016-11-29 17:05:06 -0800

    Add get mad at method

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -2,14 +2,14 @@
 
 class Santa
 
-	def initialize(santa_name, height, gender, ethnicity, age)
+	def initialize(santa_name, height, gender, ethnicity, age, reindeer_ranking)
 		# p "Initializing Santa instance ..."
 		@santa_name = santa_name
 		@height = height
 		@gender = gender
 		@ethnicity = ethnicity
 		@age = age
-		# @raindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"].sample
+		@reindeer_ranking = reindeer_ranking
 	end	
 
 	def speak
@@ -25,51 +25,92 @@ class Santa
    	 @santa_name = santa_name
   end	 
 
-  def ageofsanta
-  	@age 
+  # Release 2
+  # getter method
+  def ageofsanta(num)
+  	@age = num
+  end	
+
+  def celebrate_birthday(age)
+  	@age = age + 1
+  end	
+
+  def get_mad_at(reindeer) 
+  	
+  			p reindeer
+  			# reindeer_ranking.index(reindeer)
+
+  			@reindeer_ranking << reindeer
+  			@reindeer_ranking.delete_at(@reindeer_ranking.index(reindeer))  
+  			# p @reindeer_ranking
+
+  			
+				# p @reindeer_ranking	  	
+	end	
+
+  # ??? need a setter method here
+  def gender_reassign
+  	@gender = gender_assignment
+  end 	
+
+  # very basic getter method
+  def current_age
+  	@age
+  end	
+
+  # very basic getter method
+  def ethnicity
+  	@ethnicity
   end	
 
 end
 
 
 # Release 1
-# belly_man = Santa.new
-belly_man = Santa.new("bob", 80, "male", "irish", 0)
-belly_man.eat_milk_and_cookies("oreo")
+reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+belly_man = Santa.new("bob", 80, "male", "irish", 0, reindeer_ranking)
+p belly_man
+# belly_man.speak
+# belly_man.eat_milk_and_cookies("oreo")
+# p belly_man.ageofsanta(50)
+p belly_man.get_mad_at("Dancer")
+p belly_man
 
 
-# These two statements don't work
-# trying to write a getter method
-belly_man.ageofsanta
-# trying to print the age of santa
-belly_man.age
+# @gender to be reassigned from outside the class definition.
 
 
+# Release 2
+# age Santa by one year
+p belly_man.celebrate_birthday(40)
+
+# take a reindeer's name as an argument, and move that reindeer in last place in the reindeer rankings
+#p get_mad_at("Vixen")
+
+
+#santas[i].celebrate_birthday(age)
+# santas[i].celebrate_birthday(age)
 
 
 
 # santas = []
-# santa_name = ["Hairy Hal", "Belly Bob", "Mr. Nofearofheights", "Cody Coldnose"]
+# santa_name = ["Hairy Hal", "Belly Bob", "Mr. No_fear_of_heights", "Cody Coldnose"]
 # height = [80, 99, 87, 88]
 # gender = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
 # ethnicity = ["irish", "italian", "german", "maori", "native american"]
 # age = [0]
+# raindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+
 
 # gender.length.times do |i|
-# 	santas << Santa.new(santa_name.sample, height.sample, gender.sample, ethnicity.sample, age)
+# 	santas << Santa.new(santa_name.sample, height.sample, gender.sample, ethnicity.sample, age, raindeer_ranking)
+# 	# for each instance of Santa.new, for the age variable I want to run the celebrate_birthday method
 # end
 
 # p santas
 
-# office hours
-# why doesn't this print that last state the instance variable @santa_name?
-# instance.variable 
-# Santa:0x007f9722968ed.santa_name
-# goal is to assign the name of santa to the last santa name
-# .nameofsanta
-# p @santa_name
 
-# How to I run this and call the ageofsanta method to add?
+
 
 
 
