898ad906730869aa07d3314df27b75440ad80fb1
duffydduffyd <denise@foodfeedback.com>
2016-12-11 21:04:06 -0800

    Completed exploration

--- js/explore.js
+++ js/explore.js
@@ -5,9 +5,9 @@
 
 // // declare a string variable and set up new string variable
 
-// var myString = "Denise"
+var myString = "Denise"
 
-// var reversedString = ""
+var reversedString = ""
 
 // // take the position of the last indice of the string, which is length-1
 // // and add this to the reversed string variable 
@@ -17,12 +17,12 @@
 // // for ([initialExpression]; [condition]; [incrementExpression])
 // // statement
 
-// for (var i = myString.length-1 ; i >= 0 ; i--) {
-// 	reversedString += myString[i];
-//   }
+for (var i = myString.length-1 ; i >= 0 ; i--) {
+	reversedString += myString[i];
+  }
 
-// // print to the console the new string
-// console.log(reversedString);
+// print to the console the new string
+console.log(reversedString);
 
 
 
@@ -30,34 +30,35 @@
 
 // In function form:
 
-// Create an empty string that will host the new string
-
-// take the position of the last character index of the string, which is length-1
-// and add this to the reversed string variable 
-
-// iterate over the string subtracting one from the index until it reaches the 0 position
-
-// var reversedString = function(string) { for (var i = string.length-1 ; i >= 0 ; i--) 
-// 	reversedString += string[i];
-// 	console.log(reversedString);
-// }
-
-// reversedString("pizza")
-
-
 // Question 5 
 
-var reversedString = function(string) { for (var i = string.length-1 ; i >= 0 ; i--) 
-	reversedString += string[i];
-}
 
-reversedString("pizza")
+function backwardsString(string) {
+	
+	// Create an empty string that will host the new string
+	var reversedString = "";
 
-myResult = reversedString("pizza")
-
-
-if (1 == 1) { console.log(myResult); 
-}
-else {
+	// take the position of the last character index of the string, which is length-1
+	// and add this to the reversedString variable 
+	// iterate over the string subtracting one from the index until it reaches the 0 position
+	for (var i = string.length-1; i >= 0 ; i--) 
+		reversedString += string[i];
+	
+	// conditional print statement
+	if (1 == 1) { console.log(reversedString); 
+	}
+	else {
     console.log("The condition is false");
-} 
+	} 
+}
+
+// driver code
+newString = backwardsString("hello")
+
+
+
+// output:
+// js (javasunday) :> node explore.js
+// olleh
+
+
