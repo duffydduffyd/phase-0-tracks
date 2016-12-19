cdec79cc0d3c20235d2ab49cc1e7d738a502b9f2
Denise Duffy <denise@foodfeedback.com>
2016-12-18 20:17:01 -0800

    Add some user input areas

--- databases/uglysweater/uglysweater.rb
+++ databases/uglysweater/uglysweater.rb
@@ -8,9 +8,7 @@ require "colorate"
 
 # CREATE SQLITE3 DATABASE TO LIST SWEATER ATTRIBUTES AND TAKE IN USER SWEATER DETAILS
 
-uglydb = SQLite3::Database.new("uglysweater.db")
-uglydb.results_as_hash = true
-    
+uglydb = SQLite3::Database.new("uglysweater.db")    
 
 # CREATE: COLORS TABLE, PATTERNS TABLE, OBJECTS TABLE, PICTURES TABLE, SAYINGS TABLE 
 
@@ -82,7 +80,7 @@ uglydb.execute(create_patterns_table_cmd)
 uglydb.execute(create_sayings_table_cmd)
 uglydb.execute(create_names_table_cmd)
 
-# db.execute("SELECT * FROM names")
+
 
 
 # FILL IN THESE ATTRIBUTES TABLES IN RUBY
@@ -128,13 +126,101 @@ uglydb.execute ("INSERT INTO sayings (saying) VALUES ('Merry Christmas')")
 # # INSERT ONE NAME AND SWEATER DETAILS INTO NAMES TABLE FOR TESTING PURPOSES
 uglydb.execute ("INSERT INTO names (name, size, color_id, pattern_id, object_id, picture_id, saying_id) VALUES ('Amy Ainsworth', 'small', 1, 2, 1, 2, 1)")
 
-# ADD TEST INTERFACE TO ADD FIRST UGLY SWEATER
 
-# ADD MORE SWEATERS
+# ADD NAME AND SIZE TO THE TABLE
+puts "What is your name?"
+sweater_owner_name = gets.chomp
 
-# ADD USER INTERFACE TO ADD EVEN MORE SWEATER
+puts "What is your size?"
+sweater_owner_size = gets.chomp
 
-# RETURN THE SWEATER ATTRIBUTES TO THE CONSOLE USING THE SAME COLOR TEXT AS THE COLOR OF THEIR SWEATER
+# ITERATE THROUG THE TABLES TO SHOW USER WHAT IS THERE
+
+# ITERATE THROUGH THE COLORS TABLE
+color_array= uglydb.execute ("SELECT * FROM colors")
+
+color_array.each do |x|
+	puts x
+	puts ("----")
+end
+
+# ASK THE USER FOR THE COLOR OF THEIR SWEATER
+puts "Which number is the color of your sweater?"
+sweater_color = gets.chomp
+
+# ITERATE THROUGH THE OBJECT TABLE
+objects_array= uglydb.execute ("SELECT * FROM objects")
+
+objects_array.each do |x|
+	puts x
+	puts ("----")
+end
+
+# ASK THE USER FOR ANY OBJECTS ON THEIR SWEATER
+puts "Are there any 3D objects on your sweater? If so, which number?"
+sweater_object = gets.chomp
+
+
+# ITERATE THROUGH THE PICTURES TABLE
+pictures_array= uglydb.execute ("SELECT * FROM pictures")
+
+pictures_array.each do |x|
+	puts x
+	puts ("----")
+end
+
+# ASK THE USER FOR ANY PICTURES ON THEIR SWEATER
+puts "Are there any pictures on your sweater? Which one? Pick a number."
+sweater_picture = gets.chomp
+
+# ITERATE THROUGH THE PATTERNS TABLE
+patterns_array= uglydb.execute ("SELECT * FROM patterns")
+
+patterns_array.each do |x|
+	puts x
+	puts ("----")
+end
+
+# ASK THE USER FOR ANY PATTERNS ON THEIR SWEATER
+puts "Are there any patterns on your sweater? Which one? Pick a number."
+sweater_pattern = gets.chomp
+
+# ITERATE THROUGH THE SAYINGS TABLE
+sayings_array= uglydb.execute ("SELECT * FROM sayings")
+
+sayings_array.each do |x|
+	puts x
+	puts ("----")
+end
+
+# ASK THE USER FOR ANY SAYINGS ON THEIR SWEATER
+puts "Are there any sayings on your sweater? Which one? Pick a number."
+sweater_saying = gets.chomp
+
+
+# RETURN THE SWEATER ATTRIBUTES TO THE CONSOLE USING THE SAME COLOR AS THE COLOR OF THEIR SWEATER
+# SAMPLE, THIS WORKS
+puts Colorate.green("Your sweater is green.")
+
+# THIS DOESN'T WORK FOR SOME REASON
+ if sweater_color == 1 
+ 	puts Colorate.red("Your sweater is red.")
+ elsif sweater_color == 2
+ 	puts Colorate.green("Your sweater is green.")
+ elsif sweater_color == 3 
+ 	puts Colorate.yellow("Your sweater is yellow.")
+ elsif sweater_color == 4
+ 	puts Colorate.magenta("Your sweater is magenta.")		 	
+ elsif sweater_color == 5
+ 	puts Colorate.white("Your sweater is white.")	
+ elsif sweater_color == 6
+ 	puts Colorate.blue("Your sweater is blue.")	
+ else sweater_color == 7
+ 	puts Colorate.cyan("Your sweater is cyan.")	
+ end	
+
+# ADD THE SWEATERS INTO DATABASE, THIS IS NOT WORKING
+uglydb.execute ("INSERT INTO names (name, size, color_id, pattern_id, object_id, picture_id, saying_id) VALUES (sweater_owner_name, sweater_owner_size, sweater_color, sweater_pattern, sweater_object, sweater_picture, sweater_saying)")
 
 # COMPARE CURRENT ENTRY TO OTHER SWEATERS
 
